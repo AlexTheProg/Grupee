d381a1424c7492e3731692984712723ac863a808
Alexandru Contu <alexandru.contu99@gmail.com>
2020-10-25 08:02:49 -0700

    Initial Commit

--- app/src/main/java/com/example/grupee/MainActivity.java
+++ app/src/main/java/com/example/grupee/MainActivity.java
@@ -3,10 +3,6 @@ package com.example.grupee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void clickFunction(View buttonView){
-        EditText nameEditText = findViewById(R.id.personNameTextField);
-        Log.i("InfoLog", "Hello there friends");
-        Log.i("Values", nameEditText.getText().toString());
-
-        Toast.makeText(this, nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
-    }
 
 }
