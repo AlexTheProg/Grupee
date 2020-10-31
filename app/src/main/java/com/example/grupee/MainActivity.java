a7bdc6c1df930e43c04a87b709e2d905c0c0ea3e
Alexandru Contu <alexandru.contu99@gmail.com>
2020-10-31 05:49:43 -0700

    Initial Commit

--- app/src/main/java/com/example/grupee/MainActivity.java
+++ app/src/main/java/com/example/grupee/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.grupee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_TIMER = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView splashScreen = findViewById(R.id.splashScreen);
+        splashScreen.animate().alpha(4000F).setDuration(0);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this,LoginScreen.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },SPLASH_DISPLAY_TIMER);
     }
-
-
 }
